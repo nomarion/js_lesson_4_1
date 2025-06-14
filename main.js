7522feea4a55d93d7204b1610c392e140e62ec67
nomarion <macmini@MBP-Igor.netis.cc>
2025-06-14 10:12:58 +0300

    Рефакторинг

--- main.js
+++ main.js
@@ -1,31 +1,34 @@
 'use strict';
 
-const WHAT_YOUR_NAME = 'Как тебя зовут?';
-const EXIT = 'Все? Жаль(';
-const EMPTY_NAME = 'Так не пойдет! Надо указать имя!';
-const NUMBER_NAME = 'Не поможе на имя. Попробуй еще разок!';
+const WHAT_YOUR_NAME_MESSAGE = 'Как тебя зовут?';
+const EXIT_MESSAGE = 'Все? Жаль(';
+const EMPTY_NAME_ERROR_MESSAGE = 'Так не пойдет! Надо указать имя!';
+const NUMBER_NAME_ERROR_MESSAGE = 'Не похоже на имя. Попробуй еще разок!';
 
-const name = () => {
-    const userName = prompt(WHAT_YOUR_NAME);
+const helloMessage = (name) => `Hello, ${name}! How are you?`
+
+const askNameAndSayHi = () => {
+    const userName = prompt(WHAT_YOUR_NAME_MESSAGE);
 
     if (userName === null) {
-        alert(EXIT);
+        alert(EXIT_MESSAGE);
         return;
     }
+
     if (userName.trim() === '') {
-        alert(EMPTY_NAME);
-        name()
+        alert(EMPTY_NAME_ERROR_MESSAGE);
+        return askNameAndSayHi()
     }
 
     if (!checkString(userName)) {
-        alert(NUMBER_NAME)
-        name()
+        alert(NUMBER_NAME_ERROR_MESSAGE)
+        return askNameAndSayHi()
     }
-    alert(`Hello, ${userName}! How are you?`)
+    alert(helloMessage(userName));
 }
 
 const checkString = (value) => {
-    return /[a-zA-Zа-яА-ЯёË]/.test(value)
+    return /^[a-zA-Zа-яА-ЯёË]{3,}$/.test(value)
 }
 
-name()
+askNameAndSayHi()
